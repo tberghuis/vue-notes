79e1bb18c08f0fda2c26dbd680b37211949e9514
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-02-14 15:51:41 +1100

    .

---
+++ src/components/NoteList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>notes list</div>
+</template>
+
+<script>
+export default {
+  name: "NoteList",
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
+// todo remove Home
+
+
 const routes = [
   {
     path: "/",
