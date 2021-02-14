5bc47663f30c12459ec102b1ed7e510bfc8859b8
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-02-14 18:01:50 +1100

    .

---
+++ src/components/NewNoteButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="newNote">New Note</button>
+</template>
+
+<script>
+export default {
+  name: "NewNoteButton",
+  setup() {
+    console.log("hello setup");
+
+    function newNote(index) {
+      console.log("new note");
+    }
+
+    return {
+      newNote,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <NoteList></NoteList>
-  
+  <NewNoteButton></NewNoteButton>
 </template>
 
 <script>
 // @ is an alias to /src
 
 import NoteList from "@/components/NoteList.vue";
+import NewNoteButton from "@/components/NewNoteButton.vue";
 
 export default {
   name: "Home",
   components: {
     NoteList,
+    NewNoteButton,
   },
 };
 </script>
