80e82b8b3e0fb3a4bf3264a37d6e522939786837
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-02-15 09:28:00 +1100

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,15 @@
-// import { createStore } from "vuex";
+import { createStore } from "vuex";
 
-// export default createStore({
-//   state: {},
-//   mutations: {},
-//   actions: {},
-//   modules: {}
-// });
+export default createStore({
+  // state: {
 
-import { ref, reactive } from "vue";
-
-// export const noteStore = ref({ a: "dummy note" });
-
-const store = { a: "this is dummy store" };
-
-// export const noteStore = reactive({ a: "dummy note" });
-export const noteStore = reactive(store);
-
-export function updateNote(noteText) {
-  // noteStore.value.a = noteText;
-  noteStore.a = noteText;
-}
-
-window.noteStore = noteStore;
-window.updateNote = updateNote;
-window.store = store;
+  // },
+  state() {
+    return {
+      noteData: { a: "first note vuex" },
+    };
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+});
--- src/views/EditNote.vue
+++ src/views/EditNote.vue
@@ -6,20 +6,15 @@
 </template>
 
 <script>
-import { noteStore } from "../store";
+import store from "../store";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
 
 export default {
   setup(props) {
     const route = useRoute();
-    // console.log("value", noteStore.value[route.params.noteId]);
-
-    // const noteText
 
     return {
-      // noteText: noteStore.value[route.params.noteId],
-      noteText: noteStore[route.params.noteId],
+      noteText: store.state.noteData.a,
     };
   },
 };
