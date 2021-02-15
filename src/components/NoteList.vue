0cf784ca79b0bed58764cb3361c671dbf21ed33f
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-02-15 13:18:14 +1100

    .

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,10 +1,32 @@
 <template>
   <div>notes list</div>
+
+  <ul>
+    <li v-for="id in Object.keys(noteData)" :key="id" @click="noteClick(id)">
+      {{ noteData[id] }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import store from "../store";
+import { useRouter } from "vue-router";
+
 export default {
   name: "NoteList",
+
+  setup() {
+    const router = useRouter();
+    function noteClick(noteId) {
+      // console.log("note click id", noteId);
+      router.push(`/note/${noteId}`);
+    }
+
+    return {
+      noteData: store.state.noteData,
+      noteClick,
+    };
+  },
 };
 </script>
 
